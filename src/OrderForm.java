ffe5dfcd6007951ab18629df8819b77725b30fff
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2016-04-13 08:12:00 -0400

    Moved file I/O for orders to order class.

--- src/OrderForm.java
+++ src/OrderForm.java
@@ -1,3 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 /* ************************************
 * File: GUI.java
 * Assignment: 
@@ -22,6 +27,8 @@ public class OrderForm {
 	// on campus variables
 	private String campusLocation;
 	private String campusTime;
+	
+	private Inventory inventory;
 
 	public String getShipStreet() {
 		return shipStreet;
@@ -71,6 +78,70 @@ public class OrderForm {
 		this.campusTime = campusTime;
 	}
 	
-	
+public void storeItems() {
+		
+		BufferedWriter writer = null; // initializes the writer
+		
+		inventory = new Inventory();
+		
+		try{
+			writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream("Inventory.txt"), "utf-8"));
+			
+			writer.write(inventory.replyEnv.getItemName() + " " + inventory.replyEnv.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.window10.getItemName() + " " + inventory.window10.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.admitEnv.getItemName() + " " + inventory.admitEnv.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.admitFold.getItemName() + " " + inventory.admitFold.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.maps.getItemName() + " " + inventory.maps.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.dbPcPennant.getItemName() + " " + inventory.dbPcPennant.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.dbPennant.getItemName() + " " + inventory.dbPennant.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.engPoster.getItemName() + " " + inventory.engPoster.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.intGradBroch.getItemName() + " " + inventory.intGradBroch.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.intGradList.getItemName() + " " + inventory.intGradList.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.inqCards.getItemName() + " " + inventory.inqCards.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.lanyard.getItemName() + " " + inventory.lanyard.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.magEnv.getItemName() + " " + inventory.magEnv.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.natSecurPoster.getItemName() + " " + inventory.natSecurPoster.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.pilotPens.getItemName() + " " + inventory.pilotPens.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.presFold.getItemName() + " " + inventory.presFold.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.props.getItemName() + " " + inventory.props.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.transferBro.getItemName() + " " + inventory.transferBro.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.travPiece.getItemName() + " " + inventory.travPiece.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.viewBook.getItemName() + " " + inventory.viewBook.getReqAmount());
+			writer.newLine();
+			writer.write(inventory.viewBkEnv.getItemName() + " " + inventory.viewBkEnv.getReqAmount());
+			writer.newLine();
+			
+		} catch (IOException e){
+			 
+			e.printStackTrace();
+		}finally{
+			
+			try{writer.close();} catch (Exception e)
+			{e.printStackTrace();}
+			
+		}
+		
+		
+	}
 
 }
